287f1bca7addb9f93e72a5122faa356e80178de9
VoktexYT <ubguertin@gmail.com>
2024-09-28 13:42:45 -0400

    Install Asio,Crow package and Make Hello World API

---
+++ Main/Server/server.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "server.h"
+
+int main()
+{
+    SERVER_QUEST::CrowServer Cs;
+    Cs.start();
+
+    return 0;
+}
--- Main/Server/server.h
+++ Main/Server/server.h
@@ -1,20 +1,41 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#define PORT 7192
+#define ROOT_URL "/"
+
+
 #include <iostream>
+
 #include "../WebScraping/webscraping.h"
+#include "./Crow/include/crow.h"
+
+
 
 namespace SERVER_QUEST
 {
-	class Server
+	class CrowServer
 	{
-		public:
-			void display()
-			{
-				WEB_SCRAPING::WebScraping ws;
-				std::string data = ws.get_data();
+		private:
+			crow::SimpleApp app;
 
-				std::cout << data << std::endl;
+			void send_hello_word()
+			{
+				CROW_ROUTE(app, ROOT_URL) 
+				([]() {
+					return "Hello World";
+				});
+			}
+
+		public:
+			CrowServer()
+			{
+				send_hello_word();
+			}
+
+			void start()
+			{
+				app.port(PORT).multithreaded().run();
 			}
 	};
 }
