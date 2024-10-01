e7d93a08f2e539e37474b818a53acfaad7f0df1c
VoktexYT <ubguertin@gmail.com>
2024-09-30 22:02:43 -0400

    Add get-all-html API. getUrlRequest is created for make callback if the request is accepted or rejected

--- Main/Server/server.h
+++ Main/Server/server.h
@@ -5,9 +5,11 @@
 #define ROOT_URL "/"
 #define SHOW_USERNAME_URL "/show/<path>"
 
+#define GET_ALL_HTML_URL "/get-all-html"
+
 
 #include <iostream>
-
+#include <functional>
 #include "../WebScraping/webscraping.h"
 #include "./Crow/include/crow.h"
 
@@ -15,6 +17,25 @@
 
 namespace SERVER_QUEST
 {
+	std::string getUrlRequest(
+		crow::request req,
+		const std::function<std::string(std::string)>& callback_done,
+		const std::function<std::string()>& callback_error
+	){
+		const std::string URL_PARAM_NAME = "url";
+		const auto URL_VALUE = req.url_params.get(URL_PARAM_NAME);
+
+		if (URL_VALUE)
+		{
+			const std::string URL_VALUE_STRING = std::string(URL_VALUE);
+			callback_done(URL_VALUE_STRING);
+
+			return callback_done(URL_VALUE_STRING);
+		}
+
+		return callback_error();
+	}
+
 	class CrowServer
 	{
 		private:
@@ -42,11 +63,30 @@ namespace SERVER_QUEST
 				});
 			}
 
+			/*
+			* Get all html from web page API (Using url param)
+			*/
+			void get_all_html()
+			{
+				CROW_ROUTE(app, GET_ALL_HTML_URL)
+				([](const crow::request &req) {
+					return getUrlRequest(req, 
+						[](std::string url) {
+							return std::string("The request is accepted");
+						},
+						[]() {
+							return std::string("The request is rejected");
+						}
+					);
+				});
+			}
+
 		public:
 			CrowServer()
 			{
 				send_hello_word();
 				hello_user();
+				get_all_html();
 			}
 
 			void start()
